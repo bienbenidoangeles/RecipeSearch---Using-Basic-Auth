4954073a81b0cc3ddc293d16cd0f20028b022498
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-12-10 11:57:23 -0500

    added secrets folder

---
+++ RecipeSearch/Secrets/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  RecipeSearch
+//
+//  Created by Bienbenido Angeles on 12/10/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+struct SecretKey {
+    
+}
